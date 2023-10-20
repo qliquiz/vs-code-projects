fdcf7e3b6c6320cafa1456e3427e338412df8b77
qliquiz <artem.gorev.05@mail.ru>
2023-10-20 22:07:15 +0800

    kn

--- vs code projects/burgershit/scripts/script.js
+++ vs code projects/burgershit/scripts/script.js
@@ -27,8 +27,26 @@ document.getElementById('order-action').onclick = () => {
     }
 }
 
-/* document.getElementById('change-currency').onclick = (e) => {
+let prices = document.getElementsByClassName('products-item-price');
+document.getElementById('change-currency').onclick = (e) => {
     let currentCurrency = e.target.innerText;
+    let newCurrency = '$';
+    let coefficient = 1;
+    
+    if (currentCurrency === '$') {
+        newCurrency = '₽';
+        coefficient = 80;
+    } else if (currentCurrency === '₽') {
+        newCurrency = 'BYN';
+        coefficient = 3;
+    } else if (currentCurrency === 'BYN') {
+        newCurrency = '€';
+        coefficient = 0.9;
+    } else if (currentCurrency === '€') {
+        newCurrency = '¥';
+        coefficient = 6.9;
+    }
+    e.target.innerText = newCurrency;
 
-    let newCurrency
-} */
+    for (let i = 0; i < prices.length; i++) prices[i].innerText = +(prices[i].getAttribute('data-base-price') * coefficient).toFixed(1) + ' ' + newCurrency;
+}
