a685ff973e9749ba44796a7979d67a94fbdf10c0
qliquiz <artem.gorev.05@mail.ru>
2023-10-23 23:23:45 +0800

    hj

--- projects/burgers/src/app/app.component.ts
+++ projects/burgers/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { AppService } from './app.service';
 
@@ -10,6 +10,10 @@ import { AppService } from './app.service';
 
 export class AppComponent {
   currency = '$';
+  loaderShowed = true;
+  loader = true;
+  mainImageStyle: any;
+  orderImageStyle: any;
 
   form = this.fb.group({
     order: ['', Validators.required],
@@ -21,7 +25,16 @@ export class AppComponent {
 
   constructor(private fb: FormBuilder, private appService: AppService) {}
 
+  @HostListener('document.mousemove', ['$event'])
+  onMouseMove(e: MouseEvent) {
+    this.orderImageStyle = {transform: 'translate(-' + ((e.clientX * 0.3) / 8) + 'px,-' + ((e.clientY * 0.3) / 8) + 'px)'};
+    this.mainImageStyle = {transform: 'translate(-' + ((e.clientX * 0.3) / 8) + 'px,-' + ((e.clientY * 0.3) / 8) + 'px)'};
+  }
+
   ngOnInit() {
+    setTimeout(() => { this.loaderShowed = false, 3000 });
+    setTimeout(() => { this.loader = false, 4000 });
+    
     this.appService.getData().subscribe(data => this.productsData = data);
   }
 
