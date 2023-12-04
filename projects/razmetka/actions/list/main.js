f17fcd2baaf3edccb79c4470a36419e1bb2c8ef5
qliquiz <artem.gorev.05@mail.ru>
2023-12-04 15:28:26 +0800

    hg

--- projects/razmetka/actions/list/main.js
+++ projects/razmetka/actions/list/main.js
@@ -1,22 +1,4 @@
-let lis = document.querySelectorAll('li');
-
-for (let li of lis){
-    li.onclick = () => {
-        let childList = li.querySelector('ul');
-        childList.toggleAttribute('.hidden');
-    }
-}
-/* document.getElementById('ul').addEventListener('click', function(event) {
-    const target = event.target;
-    if (target.tagName === 'li') {
-        const childList = target.querySelector('ul');
-        if (childList) {
-            if (childList.style.display === 'none') {
-                childList.style.display = 'block';
-            } else {
-                childList.style.display = 'none';
-            }
-        }
-    }
+document.getElementById('ul').addEventListener('click', function(event) {
+    const childList = event.target.querySelector('ul');
+    childList.classList.toggle('hidden');
 });
- */
