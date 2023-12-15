c17c26dbb2243b2618067a6f4c9e6088e3de1c09
qliquiz <artem.gorev.05@mail.ru>
2023-12-15 09:41:06 +0800

    евл

--- projects/webcademy-js-6-hours-course/main.js
+++ projects/webcademy-js-6-hours-course/main.js
@@ -243,9 +243,6 @@ const myPromise = new Promise((resolve, reject) => {
     setTimeout(() => {
         // сделали какой-то запрос на сервер
         const response = true; // получили какой-то ответ
-        if (response) {
-            resolve('OK');
-        } else {
-            reject('NOT OK');
-        }
+        if (response) resolve('OK');
+        else reject('NOT OK');
     }, 1000)}).then((data) => { console.log(data) }).catch((data) => { console.log(data) });
