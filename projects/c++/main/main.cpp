6fe7457a3d4ffd5399ef0318beaf73decec9866e
qliquiz <artgor005@gmail.vom>
2024-01-06 23:36:31 +0800

    щор

--- projects/c++/main/main.cpp
+++ projects/c++/main/main.cpp
@@ -4,9 +4,18 @@ using namespace std;
 
 int main()
 {
-    setlocale(0, "");
+    setlocale(LC_ALL, "Rus");
 
     // TODO
 
     return 0;
 }
+
+/* Основные функции и типы при работе с файлами.
+<fstream> – подключаемый файл
+ifstream – тип «входной файл»
+ofstream – тип «выходной файл»
+in – имя переменной типа ifstream
+in.is_open() – был ли файл открыт
+in.fail() – была ли ошибка при последней операции
+in.eof() – закончился ли файл */
