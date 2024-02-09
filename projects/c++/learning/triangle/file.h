bd777f267e2c324691fa48292d7f389312c76f62
qliquiz <artgor005@gmail.vom>
2024-02-09 20:56:24 +0800

    z

--- projects/c++/learning/triangle/file.h
+++
@@ -1,8 +0,0 @@
-#include "geometry.h"
-
-#ifndef _FILE_H
-#define _FILE_H
-int countPoints(const char* fileName);
-bool readPoints(const char* fileName, Point* pointArray, int maxPointNum);
-bool writeTriangles(const char* fileName, Triangle* trArray, int trNum);
-#endif 
